de6d1299801859004f0b78b68828af3440a572b4
Javier Rey de Viñas <javier@iahorro-Javier.Rey>
2021-02-14 18:14:15 +0100

    fix: env variables in config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   'dev': {
-    'username': 'dbadmin',
-    'password': 'JjdorA4dEt5Man_sdlF',
-    'database': 'postgres',
-    'host': "udagram-project4-microservices.cdnyj1kcpjyt.eu-west-1.rds.amazonaws.com",
+    'username': process.env.DB_USERNAME,
+    'password': process.env.DB_PASSWORD,
+    'database': process.env.DB_NAME,
+    'host': process.env.DB_HOST,
     'dialect': "postgres",
-    'aws_region': 'eu-west-1',
-    'aws_profile': 'default',
-    'aws_media_bucket': 'udagram-jrey-dev',
+    'aws_region': process.env.AWS_REGION,
+    'aws_profile': process.env.AWS_PROFILE,
+    'aws_media_bucket': process.env.AWS_BUCKET,
     'url': 'http://localhost:8080',
   },
   // 'prod': {
